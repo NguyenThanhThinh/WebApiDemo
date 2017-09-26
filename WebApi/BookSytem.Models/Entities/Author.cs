4b179615402e33bf62a5535b7efaceb2b573dcf7
thanhthinhcntt <thanhthinhcntt@gmail.com>
2017-09-27 03:21:12 +0700

    add entities

---
+++ WebApi/BookSytem.Models/Entities/Author.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookSytem.Models.Entities
+{
+   public class Author
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        public virtual ICollection<Book> Books { get; set; } = new List<Book>();
+    }
+}
---
+++ WebApi/BookSytem.Models/Entities/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace BookSytem.Models.Entities
+{
+    using System.ComponentModel.DataAnnotations;
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public virtual ICollection<Book> Books { get; set; } = new List<Book>();
+    }
+}
