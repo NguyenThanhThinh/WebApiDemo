63e188004ae12f46ef9eb8c7590f46ea29b48a4f
thanhthinhcntt <thanhthinhcntt@gmail.com>
2017-09-27 03:40:01 +0700

    add migratons

---
+++ WebApi/BookSytem.Data/BookContext.cs
@@ -0,0 +1,55 @@
+﻿using System.Threading.Tasks;
+
+namespace BookSytem.Data
+{
+    using BookSytem.Models.Entities;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using System.Data.Entity;
+    using System.Security.Claims;
+
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager, string authenticationType)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, authenticationType);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+
+    public class BookContext : IdentityDbContext<ApplicationUser>
+    {
+        public BookContext() : base("BookContext")
+        {
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<BookContext>());
+        }
+
+        public static BookContext Create()
+        {
+            return new BookContext();
+        }
+
+        public IDbSet<Book> Books { get; set; }
+
+        public IDbSet<Category> Categories { get; set; }
+
+        public IDbSet<Author> Authors { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>()
+                .HasMany(book => book.RelatedBooks)
+                .WithMany()
+                .Map(configuration =>
+                {
+                    configuration.MapLeftKey("BookId");
+                    configuration.MapRightKey("RelatedBookId");
+                    configuration.ToTable("BooksRelatedBooks");
+                });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
