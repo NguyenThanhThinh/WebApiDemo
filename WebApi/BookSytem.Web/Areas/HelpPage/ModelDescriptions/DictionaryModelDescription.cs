50fc94fcf66990174f9603994e38b4912302d582
thanhthinhcntt <thanhthinhcntt@gmail.com>
2017-09-27 03:04:38 +0700

    add project

---
+++ WebApi/BookSytem.Web/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BookSytem.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ WebApi/BookSytem.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BookSytem.Web.WebApiApplication" Language="C#" %>
